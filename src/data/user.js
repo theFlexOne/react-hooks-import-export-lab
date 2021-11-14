9d4c7c56ce1bfbad37ecd4b11759158dbdbfedf8
Eric Hafvenstein <81645330+theFlexOne@users.noreply.github.com>
2021-11-13 20:42:54 -0600

    assignment complete

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,4 @@
-const username = "Liza";
-const city = "New York";
+export const username = 'Liza';
+export const city = 'New York';
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
-
-// export all three variables as named exports
+export const image = 'https://i.imgur.com/mV8PQxj.gif';
